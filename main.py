a25b05721836094826f87e72435507533bb66236
MeltedButter77 <meltedbuttergaming@gmail.com>
2025-02-28 21:52:04 +1030

    Added step-3 content

--- main.py
+++ main.py
@@ -16,12 +16,29 @@ body = [
 
 next_direction = "right"
 
+move_event = pygame.USEREVENT + 1
+pygame.time.set_timer(move_event, 250)
+
 while True:
     # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+
+        # Move snake forward
+        if event.type == move_event:
+            body.pop()
+            body.insert(0, body[0].copy())
+            if next_direction == "right":
+                body[0].x = body[0].x + grid_size
+            if next_direction == "left":
+                body[0].x = body[0].x - grid_size
+            if next_direction == "up":
+                body[0].y = body[0].y - grid_size
+            if next_direction == "down":
+                body[0].y = body[0].y + grid_size
+            current_direction = next_direction
 
     # Rendering
     screen.fill("black")
