42c80e0957ff30d1994d1e832b855e46abafbe98
MeltedButter77 <meltedbuttergaming@gmail.com>
2025-02-28 21:00:56 +1030

    Added content for step-1

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+# Window setup
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Snake')
+clock = pygame.time.Clock()
+fps = 60
+
+while True:
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    # Rendering
+    screen.fill("black")
+
+    # Update Screen
+    pygame.display.flip()
+    clock.tick(fps)
